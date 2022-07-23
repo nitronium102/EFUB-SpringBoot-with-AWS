69d2ef10a0af787f528e2cedade616ae6b02557e
MINJU-KIMmm <0112km@ewhain.net>
2022-07-23 23:24:39 +0900

    [1장] 인텔리제이로 스프링 부트 시작하기
    - 프로젝트 initialization
    - .gitignore 추가

---
+++ minju/src/main/java/com/efub/minju/MinjuApplication.java
@@ -0,0 +1,13 @@
+package com.efub.minju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinjuApplication.class, args);
+    }
+
+}
