f305d56fd39ef9ca3d0ae18ac1f1f57fc16bc5f8
MINJU-KIMmm <0112km@ewhain.net>
2022-07-24 21:48:13 +0900

    [3장] Spring Data JPA 테스트 코드 작성하기

---
+++ minju/src/test/java/com/efub/minju/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.efub.minju.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest //다른 설정 없으면 H2 데이터 베이스 자동 실행
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //단위테스트가 끝날때마다 수행되는 메소드. 보통 배포 전 전체 테스트를 수행할 때 테스트 간 데이터 침범을 막기 위해 사용
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() //insert/update 쿼리 실행(id있으면 update, 없으면 insert)
+        .title(title)
+        .content(content)
+        .author("jojoldu@gmail.com")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
