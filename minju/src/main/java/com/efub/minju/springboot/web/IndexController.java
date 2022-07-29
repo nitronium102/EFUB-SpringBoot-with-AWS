7f41f4422dbb9a45508ad59d8070a8cee9ee1dcb
MINJU-KIMmm <0112km@ewhain.net>
2022-07-29 20:19:35 +0900

    [4장] 기본 페이지 만들기

---
+++ minju/src/main/java/com/efub/minju/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.efub.minju.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
