ec298fbb58d1d47ec40a2c660e01d2604111b4c9
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-07-31 18:52:11 +0900

    [4장] 머스테치로 화면 구성하기

--- nitronium102/src/main/java/com/spring/efub/service/posts/PostsService.java
+++ nitronium102/src/main/java/com/spring/efub/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.spring.efub.service.posts;
 
 import com.spring.efub.web.domain.posts.Posts;
 import com.spring.efub.web.domain.posts.PostsRepository;
+import com.spring.efub.web.dto.PostsListResponseDto;
 import com.spring.efub.web.dto.PostsResponseDto;
 import com.spring.efub.web.dto.PostsSaveRequestDto;
 import com.spring.efub.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,19 @@ public class PostsService {
 	public PostsResponseDto findById(Long id) {
 		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 		return new PostsResponseDto(entity);
+	}
+
+	@Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도 개선
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+				.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		Posts posts = postsRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		postsRepository.delete(posts);
 	}
 }
