cc2be14ede99ca97128ea358846d0745feb13808
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2022-07-31 19:04:54 +0900

    [4장] Mustache로 게시글 등록 화면 만들기 & 등록 DB 확인

--- gom3rye/src/main/java/com/gom3rye/book/springboot/web/IndexController.java
+++ gom3rye/src/main/java/com/gom3rye/book/springboot/web/IndexController.java
@@ -1,14 +1,21 @@
 package com.gom3rye.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+    @GetMapping("/posts/save") // 머스테치로 /posts/save 페이지 주소 만들어주었으니까 이 주소에 해당하는 컨트롤러 생성
+    // 페이지에 관련된 컨트롤러는 모두 IndexController를 사용한다.
+    public String postsSave(){
+        return "posts-save";
     }
 
 }
